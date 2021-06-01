44c5824cc0d4887d6bc0db174253dcfce51f617c
Vinod Patidar <pvinod813@gmail.com>
2021-06-01 17:31:29 +0530

    Transaction

---
+++ BankAPIApplication/Models/Transactions/TransactionRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BankAPIApplication.Entities;
+
+namespace BankAPIApplication.Models.Transactions
+{
+    
+    public class TransactionRequest
+    {
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        public string Mode { get; set; }
+    }
+}
---
+++ BankAPIApplication/Models/Transactions/TransactionResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BankAPIApplication.Models.Transactions
+{
+    public class TransactionResponse
+    {
+        public int Amount { get; set; }
+        public int TotalBalance { get; set; }
+        public string Status { get; set; }
+    }
+}
